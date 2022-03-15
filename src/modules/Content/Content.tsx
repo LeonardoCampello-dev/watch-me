d9d5487bbde3e97a0effb563ba868306f0d7fd2c
Leonardo Campello <leonardocampello.dev@gmail.com>
2022-03-15 10:17:00 -0300

    feat: implements virtualized list of movies

--- src/modules/Content/Content.tsx
+++ src/modules/Content/Content.tsx
@@ -1,26 +1,38 @@
 import { ComponentType } from 'react';
+import { List, ListRowRenderer } from 'react-virtualized';
 
 import { CategoryTitle } from './components';
 import { MovieCard } from '../MovieCard/MovieCard';
 import { ContentProps } from './Types';
 
 export const Content: ComponentType<ContentProps> = ({ selectedGenre, movies }) => {
+  const rowRenderer: ListRowRenderer = ({ index, style }) => {
+    return (
+      <div key={movies[index].imdbID} style={style}>
+        <MovieCard
+          title={movies[index].Title}
+          poster={movies[index].Poster}
+          runtime={movies[index].Runtime}
+          rating={movies[index].Ratings[0].Value}
+        />
+      </div>
+    );
+  };
+
+  const setup = {
+    height: 600,
+    rowHeight: 380,
+    width: 800,
+    overscanRowCount: 5,
+    rowCount: movies.length
+  };
+
   return (
     <div className="container">
       <CategoryTitle title={selectedGenre.title || ''} />
 
       <main>
-        <div className="movies-list">
-          {movies.map(movie => (
-            <MovieCard
-              key={movie.imdbID}
-              title={movie.Title}
-              poster={movie.Poster}
-              runtime={movie.Runtime}
-              rating={movie.Ratings[0].Value}
-            />
-          ))}
-        </div>
+        <List {...setup} rowRenderer={rowRenderer} />
       </main>
     </div>
   );
